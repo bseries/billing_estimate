774797cc6c994930eb99902420d3683fe4970382
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 21:07:22 +0200

    Include utf8mb4 migration

---
+++ data/upgrade/001-utf8mb4.sql
@@ -0,0 +1,4 @@
+ALTER TABLE billing_estimates CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE billing_estimates DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE billing_estimate_positions CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE billing_estimate_positions DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
